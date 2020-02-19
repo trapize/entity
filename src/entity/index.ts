d694a7fe6093d673b725b730f369c592a7b31d7f
Zachary Trank-Zelewicz <trank.zachary@gmail.com>
2020-02-18 20:35:37 -0500

    fixes

--- src/entity/index.ts
+++ src/entity/index.ts
@@ -22,5 +22,5 @@ export * from './query-builder';
 export * from './operators';
 export * from './exceptions/Entity.Exception';
 
-import * as Entities from './Symbols';
+import { EntitySymbols as Entities } from './Symbols';
 export { Entities }
